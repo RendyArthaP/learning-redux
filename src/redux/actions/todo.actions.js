a1a8d86193b0ea19d964c802106a1b4c09672e1a
Rendy Artha <rendyartha312@gmail.com>
2021-05-10 17:21:42 +0700

    get todos api at actions redux

--- src/redux/actions/todo.actions.js
+++ src/redux/actions/todo.actions.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const GET_TODO_REQUEST = 'GET_TODO_REQUEST';
 export const GET_TODO_SUCCESS = 'GET_TODO_SUCCESS';
 export const GET_TODO_ERROR = 'GET_TODO_ERROR';
@@ -19,5 +21,16 @@ export const getTodoError = (error) => {
   return {
     type: GET_TODO_ERROR,
     error
+  }
+}
+
+export const getTodo = () => {
+  return function(dispatch) {
+    dispatch(getTodoRequest())
+
+    axios
+      .get('https://6023a95a6bf3e6001766b546.mockapi.io/todos')
+      .then((result) => dispatch(getTodoSuccess(result.data)))
+      .catch((error) => dispatch(getTodoError(error)))
   }
 }
