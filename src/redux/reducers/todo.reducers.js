2b46f41f40866fca0d7d29f91ce8e0a646638f9c
Rendy Artha <rendyartha312@gmail.com>
2021-05-10 17:12:01 +0700

    setup get todo reducers

--- src/redux/reducers/todo.reducers.js
+++ src/redux/reducers/todo.reducers.js
@@ -5,11 +5,36 @@ import {
 } from '../../redux/actions/todo.actions.js';
 
 const initialState = {
-  data: []
+  data: [],
+  isLoading: false,
+  isError: false
 }
 
 const handleTodo = (state = initialState, action) => {
-
+  switch(action.type) {
+    case GET_TODO_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      }
+    case GET_TODO_SUCCESS:
+      return {
+        ...state,
+        data: action.result,
+        isLoading: false,
+        isError: false
+      }
+    case GET_TODO_ERROR:
+      return {
+        ...state,
+        error: action.error,
+        isLoading: false,
+        isError: true
+      }
+    default:
+      return state;
+  }
 }
 
 export default handleTodo
